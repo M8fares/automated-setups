699ed510a4035169d6426a37f9f6d9e48987436a
Paul Waldmann <paul@norsestack.com>
2018-11-10 00:41:22 +0800

    Update magento.sh

--- Ubuntu-18.04/magento.sh
+++ Ubuntu-18.04/magento.sh
@@ -37,101 +37,6 @@ apt-get -y upgrade
 # Generate MySQL password for 'root'@'localhost'
 MYSQL_PASS=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32) || true
 
-# LEMP Stack
-function lemp(){
-  # Add 'Universe' repository for php-fpm
-  apt-get -y install software-properties-common
-  apt-add-repository universe
-
-  # Set previously generated password for 'root'@'localhost'
-  debconf-set-selections <<< "mysql-server mysql-server/root_password password ${MYSQL_PASS}"
-  debconf-set-selections <<< "mysql-server mysql-server/root_password_again password ${MYSQL_PASS}"
-
-  # Install Linux, NGINX, MySQL and PHP (LEMP)
-  DEBIAN_FRONTEND=noninteractive apt-get -y install nginx mysql-server mysql-client php-fpm php-mysql
-
-  # Enable passwordless MySQL command execution
-  cat >/root/.my.cnf <<CMD_EOF
-[mysql]
-user=root
-password=${MYSQL_PASS}
-CMD_EOF
-
-  # Setting up php-fpm socket
-  PHP_VER=$(php -v | head -1 | cut -f2 -d' ' | cut -f1,2 -d.)
-  cat >/etc/nginx/conf.d/fpm.conf <<CMD_EOF
-upstream php-handler {
-server unix:///run/php/php${PHP_VER}-fpm.sock;
-}
-CMD_EOF
-
-  # Uncomment PHP section in NGINX default configuration
-  pushd /etc/nginx/sites-available
-  sed -i.save '/location ~ \\\.php\$ {/,/#}/ s/#//' default
-  sed -i.save '/location ~ \\\.php\$ {/,/#}/ {/fastcgi_pass 127/d}' default
-  sed -i.save '/location ~ \\\.php\$ {/,/#}/ s/fastcgi_pass unix.*/fastcgi_pass php-handler;/' default
-  rm -f default.save
-  popd
-
-  # Increase limits for PHP
-  cat >"/etc/php/${PHP_VER}/fpm/conf.d/99-limits.conf" <<EOF
-upload_max_filesize = 2048M
-post_max_size = 32M
-memory_limit = 256M
-EOF
-
-  # Reload NGINX and PHP service
-  systemctl reload php${PHP_VER}-fpm
-  systemctl reload nginx
-}
-
-
-# Hardening PHP for better security
-function hardening_php(){
-  PHP_VER=$(php -v | head -1 | cut -f2 -d' ' | cut -f1,2 -d.)
-  cat >"/etc/php/${PHP_VER}/fpm/conf.d/99-hardening.conf" <<CMD_EOF
-# Disable functions globally
-disable_functions = _getppid,allow_url_fopen,allow_url_include,chgrp,chmod,chown,curl_exec,curl_multi_exec,diskfreespace,dl,exec,fpaththru,getmypid,getmyuid,highlight_file,ignore_user_abord,ini_set,lchgrp,lchown,leak,link,listen,parse_ini_file,passthru,pcntl_exec,php_uname,phpinfo,popen,posix,posix_ctermid,posix_getcwd,posix_getegid,posix_geteuid,posix_getgid,posix_getgrgid,posix_getgrnam,posix_getgroups,posix_getlogin,posix_getpgid,posix_getpgrp,posix_getpid,posix_getpwnam,posix_getpwuid,posix_getrlimit,posix_getsid,posix_getuid,posix_isatty,posix_kill,posix_mkfifo,posix_setegid,posix_seteuid,posix_setgid,posix_setpgid,posix_setsid,posix_setuid,posix_times,posix_ttyname,posix_uname,proc_close,proc_get_status,proc_nice,proc_open,proc_terminate,putenv,set_time_limit,shell_exec,show_source,source,system,tmpfile,virtual
-
-# Prevent setting of undeletable session ID cookies
-session.use_strict_mode = 1
-
-# Allows access to session ID cookie only when protocol is HTTPS
-session.cookie_secure = 1
-CMD_EOF
-
-  # Limit FPM to .php extension to prevent malicious users to use other extensions to execute php code.
-  PHP_CONF="/etc/php/${PHP_VER}/fpm/pool.d/www.conf"
-  if grep -m 1 -c 'security.limit_extensions' "${PHP_CONF}" >/dev/null; then
-    sed -i.save 's/security.limit_extensions.*/security.limit_extensions = .php/' "${PHP_CONF}"
-  else
-    echo 'security.limit_extensions = .php' >> "${PHP_CONF}"
-  fi
-
-  PHP_CONF="/etc/php/$PHP_VER/fpm/php.ini"
-
-  # Disable PATH_INFO
-  if grep -m 1 -c 'cgi.fix_pathinfo' "${PHP_CONF}" >/dev/nulls; then
-    sed -i.save 's/cgi.fix_pathinfo.*/cgi.fix_pathinfo = 0/' "${PHP_CONF}"
-  else
-    echo 'cgi.fix_pathinfo = 0' >> "${PHP_CONF}"
-  fi
-
-  # Disable Fopen wrappers
-  if grep -m 1 -c 'allow_url_fopen' "${PHP_CONF}" >/dev/null; then
-    sed -i.save 's/allow_url_fopen.*/allow_url_fopen = Off/' "${PHP_CONF}"
-  else
-    echo 'allow_url_fopen = Off' >> "${PHP_CONF}"
-  fi
-
-  if grep -m 1 -c 'allow_url_include' "${PHP_CONF}" >/dev/null; then
-    sed -i.save 's/allow_url_include.*/allow_url_include = Off/' "${PHP_CONF}"
-  else
-    echo 'allow_url_include = Off' >> "${PHP_CONF}"
-  fi
-}
-
-
 MG_DB='magento'
 MG_USER='magentouser'
 MG_PASS=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32) || true
@@ -139,10 +44,11 @@ MG_PASS=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32) || true
 MG_ADMIN='magentoadmin'
 MG_ADMIN_PASS=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c32) || true
 
-function install_lemp(){
-  # Needed for libmcrypt4
+function lemp(){
+  # Add 'Universe' repository for libmcrypt4
+  apt-get -y install software-properties-common
   add-apt-repository -y universe
-  # Needed for php7.0 ( magento dependency)
+  # Needed for php7.0 (magento dependency)
   add-apt-repository -y ppa:ondrej/php
   apt update -y
 
@@ -173,10 +79,10 @@ CMD_EOF
   rm -f default.save
   popd
 
-  # If you want to upload large files
-  cat >"/etc/php/${PHP_VER}/fpm/conf.d/mem.conf" <<EOF
+  # Increase limits for PHP
+  cat >"/etc/php/${PHP_VER}/fpm/conf.d/99-limits.conf" <<EOF
 upload_max_filesize = 2048M
-post_max_size = 2048M
+post_max_size = 32M
 memory_limit = 256M
 EOF
 
@@ -260,6 +166,53 @@ CMD_EOF
   popd
 }
 
+
+# Hardening PHP for better security
+function hardening_php(){
+  PHP_VER=$(php -v | head -1 | cut -f2 -d' ' | cut -f1,2 -d.)
+  cat >"/etc/php/${PHP_VER}/fpm/conf.d/99-hardening.conf" <<CMD_EOF
+# Disable functions globally
+disable_functions = _getppid,allow_url_fopen,allow_url_include,chgrp,chmod,chown,curl_exec,curl_multi_exec,diskfreespace,dl,exec,fpaththru,getmypid,getmyuid,highlight_file,ignore_user_abord,ini_set,lchgrp,lchown,leak,link,listen,parse_ini_file,passthru,pcntl_exec,php_uname,phpinfo,popen,posix,posix_ctermid,posix_getcwd,posix_getegid,posix_geteuid,posix_getgid,posix_getgrgid,posix_getgrnam,posix_getgroups,posix_getlogin,posix_getpgid,posix_getpgrp,posix_getpid,posix_getpwnam,posix_getpwuid,posix_getrlimit,posix_getsid,posix_getuid,posix_isatty,posix_kill,posix_mkfifo,posix_setegid,posix_seteuid,posix_setgid,posix_setpgid,posix_setsid,posix_setuid,posix_times,posix_ttyname,posix_uname,proc_close,proc_get_status,proc_nice,proc_open,proc_terminate,putenv,set_time_limit,shell_exec,show_source,source,system,tmpfile,virtual
+
+# Prevent setting of undeletable session ID cookies
+session.use_strict_mode = 1
+
+# Allows access to session ID cookie only when protocol is HTTPS
+session.cookie_secure = 1
+CMD_EOF
+
+  # Limit FPM to .php extension to prevent malicious users to use other extensions to execute php code.
+  PHP_CONF="/etc/php/${PHP_VER}/fpm/pool.d/www.conf"
+  if grep -m 1 -c 'security.limit_extensions' "${PHP_CONF}" >/dev/null; then
+    sed -i.save 's/security.limit_extensions.*/security.limit_extensions = .php/' "${PHP_CONF}"
+  else
+    echo 'security.limit_extensions = .php' >> "${PHP_CONF}"
+  fi
+
+  PHP_CONF="/etc/php/$PHP_VER/fpm/php.ini"
+
+  # Disable PATH_INFO
+  if grep -m 1 -c 'cgi.fix_pathinfo' "${PHP_CONF}" >/dev/nulls; then
+    sed -i.save 's/cgi.fix_pathinfo.*/cgi.fix_pathinfo = 0/' "${PHP_CONF}"
+  else
+    echo 'cgi.fix_pathinfo = 0' >> "${PHP_CONF}"
+  fi
+
+  # Disable Fopen wrappers
+  if grep -m 1 -c 'allow_url_fopen' "${PHP_CONF}" >/dev/null; then
+    sed -i.save 's/allow_url_fopen.*/allow_url_fopen = Off/' "${PHP_CONF}"
+  else
+    echo 'allow_url_fopen = Off' >> "${PHP_CONF}"
+  fi
+
+  if grep -m 1 -c 'allow_url_include' "${PHP_CONF}" >/dev/null; then
+    sed -i.save 's/allow_url_include.*/allow_url_include = Off/' "${PHP_CONF}"
+  else
+    echo 'allow_url_include = Off' >> "${PHP_CONF}"
+  fi
+}
+
+
 # Add a message to login motd, with credentials script created
 function setup_motd(){
   local MOTD_FILE='/etc/update-motd.d/99-credentials'
@@ -278,7 +231,6 @@ CMD_EOF
 
 
 lemp;
-hardening_php;
-install_lemp;
 install_magento;
+hardening_php;
 setup_motd;
